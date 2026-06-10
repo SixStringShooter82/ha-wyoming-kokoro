984df3bc7db019f4a07b33637b03ed5e03f7f533
SixStringShooter82 <montjoyjason@gmail.com>
2026-06-10 18:15:47 -0400

    Update run.sh

--- wyoming-kokoro/run.sh
+++ wyoming-kokoro/run.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-VOICE=$(bashio::config 'voice')
-SPEED=$(bashio::config 'speed')
+bashio::log.info "Starting Wyoming Kokoro TTS v2.0..."
+bashio::log.info "Multi-character emotional TTS engine"
 
-bashio::log.info "Starting Wyoming Kokoro TTS..."
-bashio::log.info "Voice: ${VOICE}"
-bashio::log.info "Speed: ${SPEED}"
-
-exec python3 -m wyoming_kokoro \
-    --voice "${VOICE}" \
-    --speed "${SPEED}" \
-    --uri tcp://0.0.0.0:10200
+exec python3 /app/server.py \
+    --uri tcp://0.0.0.0:10200 \
+    --config /data/options.json
